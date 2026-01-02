94d9fcf346bc413b61877a75b591de083600171b
AMARJOV <amarjov@alumno.upv.es@ldsic-vdi04.upvnet.upv.es>
2026-01-02 12:30:10 +0100

    Implementación de Circle y reglas de Makefile

---
+++ Circle.cpp
@@ -0,0 +1,42 @@
+#include "Circle.h"
+#include <cmath>
+
+Circle::Circle() : Shape() {
+    center = Point2D(0, 0);
+    radius = 1.0;
+}
+
+Circle::Circle(std::string color, Point2D center, double radius) : Shape(color) {
+    this->center = center;
+    this->radius = radius;
+}
+
+Point2D Circle::get_center() const { return center; }
+void Circle::set_center(Point2D p) { center = p; }
+
+double Circle::get_radius() const { return radius; }
+void Circle::set_radius(double r) { radius = r; }
+
+double Circle::area() const {
+    return 3.141592 * std::pow(radius, 2);
+}
+
+double Circle::perimeter() const {
+    return 2 * 3.141592 * radius;
+}
+
+void Circle::translate(double incX, double incY) {
+    center.x += incX;
+    center.y += incY;
+}
+
+void Circle::print() const {
+    std::cout << "[Circle: color = " << color 
+              << "; center = " << center 
+              << "; radius = " << radius << "]";
+}
+
+std::ostream& operator<<(std::ostream &out, const Circle &c) {
+    c.print();
+    return out;
+}
